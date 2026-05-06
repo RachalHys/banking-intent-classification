b42ee359b9314604964424316a6d98157a55480f
RachalHys <khuong.ai@gmail.com>
2026-05-07 01:19:15 +0700

    Add scripts run to bash

--- inference.sh
+++ inference.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+echo "====================================="
 echo "1. Single Message Inference Test"
+echo "Executing command: python scripts/inference.py --message \"I lost my credit card, please block it!\""
+echo "====================================="
 python scripts/inference.py --message "I lost my credit card, please block it!"
 
 echo ""
+echo "====================================="
 echo "2. Batch Evaluation on Test Set"
+echo "Executing command: python scripts/inference.py --eval sample_data/test.csv --save"
+echo "====================================="
 python scripts/inference.py --eval sample_data/test.csv --save
 
 echo ""
